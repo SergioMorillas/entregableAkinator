24d57f3c3933cdb6863bff23219f74aaf9cbd785
Vespertino <Vespertino@C02PC23.informatica.comercio>
2023-04-13 17:07:24 +0200

    Ya funciona perfecto y guarda los nodos al acabar. 13/04/2023

--- src/main/java/proyecto/practicaentregable/SE.java
+++ src/main/java/proyecto/practicaentregable/SE.java
@@ -1,9 +1,23 @@
 package proyecto.practicaentregable;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import static java.lang.System.exit;
 import java.util.Scanner;
+import proyecto.escrituraendisco.Fichero;
 
 /**
+ *
+ * Esta clase representa el juego "Adivina el animal". Permite al usuario pensar
+ * en un animal y el programa tratará de adivinarlo mediante preguntas que el
+ * usuario deberá responder con "Si" o "No". Si no lo adivina, el programa
+ * aprenderá una nueva pregunta que diferencie al animal del que estaba pensando
+ * y la agregará a su base de conocimientos.
+ *
  * @author Sergio Morillas
  */
 public class SE {
@@ -12,12 +26,19 @@ public class SE {
     private String respuesta, pregunta;
     private final Scanner s;
 
+    /**
+     * <b>Constructor</b> que permite crearse objetos de tipo SE utilizando un
+     * nodo raiz
+     *
+     * @param raiz Nodo creado en la clase que contiene el main para trabajar
+     * sobre el sistema experto
+     */
     public SE(Nodo raiz) {
         this.raiz = raiz;
         s = new Scanner(System.in);
     }
 
-    public void juega(Nodo nodo) {
+    public void juega(Nodo nodo) throws IOException {
         if (nodo.getPregunta() != null) { // Comprobamos si la pregunta no es null
             System.out.println(nodo.getPregunta());
             respuesta = s.nextLine();
@@ -29,11 +50,17 @@ public class SE {
         if (s.nextLine().equalsIgnoreCase("si")) {
             this.juega(raiz);
         } else {
+            guardarInformacion();
             exit(0);
         }
     }
 
-    private void comprobaciones(Nodo nodo) {
+    /**
+     * Método que inicia el juego.
+     *
+     * @param nodo Nodo actual del árbol en el que nos encontramos.
+     */
+    private void comprobaciones(Nodo nodo) throws IOException {
         if (respuesta.equalsIgnoreCase("si")) { // Si el usuario responde si a la pregunta
             if (nodo.getRespuesta() != null) { // Si hay respuesta directa la mostramos
                 System.out.println("Tu animal es " + nodo.getRespuesta());
@@ -64,13 +91,13 @@ public class SE {
         String respuestas[] = new String[3];
         if (nodo.getRespuesta() != null) {
             System.out.println("¿Que animal estabas pensando?");
-            respuestas[0] = s.nextLine();
+            respuestas[0] = s.nextLine().toLowerCase();
             System.out.println("¿Que pregunta diferenciaría a tu animal del "
                     + nodo.getRespuesta() + "?");
-            respuestas[1] = s.nextLine();
+            respuestas[1] = formatearPregunta(s.nextLine());
             System.out.println("¿Tu animal responde de manera "
                     + "afirmativa a la pregunta? (Si/No)");
-            respuestas[2] = s.nextLine();           
+            respuestas[2] = s.nextLine();
             if (respuestas[2].equalsIgnoreCase("no")) {
                 nodo.setNodoSi(new Nodo(null, nodo.getRespuesta()));
                 nodo.setNodoNo(new Nodo(null, respuestas[0]));
@@ -85,6 +112,74 @@ public class SE {
             respuestas[0] = s.nextLine();
             System.out.println("Vale, volveremos a intentarlo");
             nodo.setNodoSi(new Nodo(null, respuestas[0]));
+        }
+    }
+
+    /**
+     * Metodo que formatea las preguntas para que el usuario las pueda
+     * introducir como quiera (mayusculas, minusculas...) y sin signos de
+     * interrogación
+     *
+     * @param frase Pregunta que ha señalizado el usuario
+     * @return la frase ya formateada
+     */
+    private String formatearPregunta(String frase) {
+        char primero = frase.toUpperCase().charAt(0);
+        String resto = frase.substring(1, frase.length()).toLowerCase();
+        return "¿" + primero + resto + "?";
+    }
+
+    /**
+     * Método que recorre el árbol binario y lo guarda en un archivo de texto.
+     *
+     * @param nodo Nodo a partir del cual se comienza a recorrer el árbol.
+     * @param archivo Ruta del archivo de texto en el que se guardará el
+     * árbol.
+     * @throws IOException Si se produce un error al escribir en el archivo.
+     */
+    public void guardarArbolEnArchivo(Nodo nodo, String archivo) throws IOException {
+        try (PrintWriter escritor = new PrintWriter(new BufferedWriter(new FileWriter(archivo)))) {
+            guardarNodoEnArchivo(nodo, escritor);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    /**
+     * Método auxiliar que guarda un nodo y sus subnodos en el archivo de texto.
+     *
+     * @param nodo Nodo a guardar.
+     * @param escritor Escritor que se encargará de escribir en el archivo de
+     * texto.
+     */
+    private void guardarNodoEnArchivo(Nodo nodo, PrintWriter escritor) {
+        if (nodo == null) {
+            return;
+        }
+        if (nodo.getPregunta() != null) {
+            escritor.println("P:" + nodo.getPregunta());
+            guardarNodoEnArchivo(nodo.getNodoSi(), escritor);
+            guardarNodoEnArchivo(nodo.getNodoNo(), escritor);
+        } else {
+            escritor.println("R:" + nodo.getRespuesta());
+        }
+    }
+
+    /**
+     * Metodo que guarda la informacion de los nodos en un fichero txt
+     *
+     * @throws IOException Si el fichero no existe lo creamos y añadimos la
+     * información
+     */
+    private void guardarInformacion() throws IOException {
+        System.out.println("¿Quieres guardar la informacion de la partida?");
+        if (s.nextLine().equalsIgnoreCase("si")) {
+            try {
+                guardarArbolEnArchivo(this.raiz, "./src/main/resources/texto/guardaArbol.txt");
+            } catch (Exception e) {
+                Fichero.crearArchivo("./src/main/resources/texto", "guardaArbol.txt");
+                guardarArbolEnArchivo(this.raiz, "./src/main/resources/texto/guardaArbol.txt");
+            }
         }
     }
 }
